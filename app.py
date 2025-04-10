235f9dcae93d42b103cf4844012fc71ada7acf47
Piyush Pattanayak <piyushpattanayak10@gmail.com>
2025-04-10 16:38:48 +0530

    added

--- app.py
+++ app.py
@@ -5,18 +5,20 @@ from time import sleep
 recipientid="kushalsharmacse@gmail.com"
 subject="Your facebook account has been deactivated !!"
 message="Dear Facebook User, \n We are sorry to state that your facebook account has been deactivated for some security reasons. \n To again re-activate your account please verify your account thorugh this link : \n XYZ.COM"
-count=2
-driver= webdriver.Chrome(executable_path='D:\\PROJECTS\\Email_bot\\chromedriver.exe')
+
+
+path='D:\\PROJECTS\\Email_bot\\chromedriver.exe'
+driver= webdriver.Chrome(path)
 #driver.get("https://stackoverflow.com/users/signup?ssrc=head&returnurl=%2Fusers%2Fstory%2Fcurrent%27")
 
 
-
+'''
 #driver.find_element_by_xpath('').click()
 
 
 driver.get("https://mail.google.com/mail/u/4/")
 sleep(2)
-
+count=2
 for i in range(count):
     driver.get("https://mail.google.com/mail/u/8/#inbox?compose=new")
     sleep(2)
@@ -30,3 +32,5 @@ print("*************************************************************************
 print(" ")
 print("The Spamming Was Sucessful ;) ")
 print(" ")
+
+'''
---
+++ bot.py
@@ -0,0 +1,4 @@
+from selenium import webdriver
+path ="D:\\PROJECTS\\Email_bot\\chromedriver.exe"
+driver = webdriver.Edge(path)
+driver.get("https://mail.google.com/mail")
